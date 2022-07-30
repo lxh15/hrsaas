f6ba6f0d5c65c738909041bfbe51a615c6e20b74
lxh15 <1392649188@qq.com>
2022-07-30 17:25:18 +0800

    项目初始化完成

---
+++ src/icons/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon'// svg component
+
+// 注册全局组件
+Vue.component('svg-icon', SvgIcon)
+
+// 一次性加载svg下面的所有svg文件
+const req = require.context('./svg', false, /\.svg$/)
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+requireAll(req)
