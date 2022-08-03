eb1208f6ad58f26a6f7dcc9a6ec53763a6215f01
lxh15 <1392649188@qq.com>
2022-08-03 16:08:41 +0800

    完成了路由分支

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,15 @@ Vue.use(Router)
 
 /* Layout */
 import Layout from '@/layout'
-import { login } from '@/api/user'
-
+// 引入多个模块的规则
+import approvalsRouter from './modules/approvals'
+import departmentsRouter from './modules/departments'
+import employeesRouter from './modules/employees'
+import permissionRouter from './modules/permission'
+import attendancesRouter from './modules/attendances'
+import salarysRouter from './modules/salarys'
+import settingRouter from './modules/setting'
+import socialRouter from './modules/social'
 // 静态理由
 export const constantRoutes = [
   {
@@ -30,7 +37,7 @@ export const constantRoutes = [
         path: 'dashboard',
         name: 'Dashboard',
         component: () => import('@/views/dashboard/index'),
-        meta: { title: 'Dashboard', icon: 'dashboard' }
+        meta: { title: '首页', icon: 'dashboard' }
       }
     ]
   },
@@ -39,11 +46,23 @@ export const constantRoutes = [
   { path: '*', redirect: '/404', hidden: true }
 ]
 
+// 动态路由
+export const asyncRoutes = [
+  approvalsRouter,
+  departmentsRouter,
+  employeesRouter,
+  permissionRouter,
+  attendancesRouter,
+  salarysRouter,
+  settingRouter,
+  socialRouter
+]
+
 const createRouter = () =>
   new Router({
     // mode: 'history', // require service support
-    scrollBehavior: () => ({ y: 0 }),
-    routes: constantRoutes
+    scrollBehavior: () => ({ y: 0 }), // 管理滚动行为 如果出现滚动 切换就让 让页面回到顶部
+    routes: [...constantRoutes, ...asyncRoutes] // 临时合并所有的路由
   })
 
 const router = createRouter()
